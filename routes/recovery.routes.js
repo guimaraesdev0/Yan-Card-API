f82713811ce307ec94ad5540daf749515b9ba51f
Guimaraes <69005421+guimaraesdev0@users.noreply.github.com>
2024-07-25 11:01:41 -0300

    Add files via upload

--- routes/recovery.routes.js
+++ routes/recovery.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const RecoveryController = require('../controllers/recovery.controller');
 
-router.get('/verifyRecoveryCode', RecoveryController.verifyRecoveryCode);
+router.post('/verifyRecoveryCode', RecoveryController.verifyRecoveryCode);
 router.post('/requestRecoverypass', RecoveryController.createRecovery);
 router.post('/recoverypass', RecoveryController.changePassword);
 
