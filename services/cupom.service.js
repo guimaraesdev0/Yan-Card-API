be0ef276c4bc3d17cd167505258736add837f6f2
Guimaraes <69005421+guimaraesdev0@users.noreply.github.com>
2024-08-01 15:39:49 -0300

    Update cupom.service.js

--- services/cupom.service.js
+++ services/cupom.service.js
@@ -1,47 +1,38 @@
 const connectToDatabase = require('../config/db.config');
 
-async function getAllCupons() {
+async function getAllUsedCupons() {
     try {
         const connection = await connectToDatabase();
         const query = `
             SELECT cuponsusados.*, restaurantes.nome AS restauranteNome
             FROM cuponsusados
             JOIN restaurantes ON cuponsusados.idrestaurante = restaurantes.id
+            WHERE cuponsusados.usado = 1
         `;
-        console.log('Executing query:', query);
         const [rows] = await connection.query(query);
-        console.log('Query result:', rows);
         return rows;
     } catch (error) {
-        console.error('Error executing query:', error);
         throw new Error(error);
     }
 }
 
-async function getAllCuponsByUserId(userId) {
+async function getAllUsedCuponsByUserId(userId) {
     try {
-        if (!userId) {
-            throw new Error('userId is required');
-        }
         const connection = await connectToDatabase();
         const query = `
             SELECT cuponsusados.*, restaurantes.nome AS restauranteNome
             FROM cuponsusados
             JOIN restaurantes ON cuponsusados.idrestaurante = restaurantes.id
-            WHERE cuponsusados.idcliente = ?
+            WHERE cuponsusados.idcliente = ? AND cuponsusados.usado = 1
         `;
-        console.log('Executing query with userId:', userId);
-        console.log('Query:', query);
         const [rows] = await connection.query(query, [userId]);
-        console.log('Query result:', rows);
         return rows;
     } catch (error) {
-        console.error('Error executing query:', error);
         throw new Error(error);
     }
 }
 
 module.exports = {
-    getAllCupons,
-    getAllCuponsByUserId
+    getAllUsedCupons,
+    getAllUsedCuponsByUserId
 };
