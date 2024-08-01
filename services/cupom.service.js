fa730725bc4118aeca53693d59f260e4a1d47184
Guimaraes <69005421+guimaraesdev0@users.noreply.github.com>
2024-08-01 15:26:15 -0300

    Update cupom.service.js

--- services/cupom.service.js
+++ services/cupom.service.js
@@ -3,25 +3,36 @@ const connectToDatabase = require('../config/db.config');
 async function getAllUsedCupons() {
     try {
         const connection = await connectToDatabase();
-        const [rows] = await connection.query(`SELECT * FROM cuponsusados`)
+        const query = `
+            SELECT cuponsusados.*, restaurantes.nome AS restauranteNome
+            FROM cuponsusados
+            JOIN restaurantes ON cuponsusados.idrestaurante = restaurantes.id
+            WHERE cuponsusados.usado = 1
+        `;
+        const [rows] = await connection.query(query);
         return rows;
     } catch (error) {
         throw new Error(error);
     }
-
 }
 
 async function getAllUsedCuponsByUserId(userId) {
     try {
-       const connection = await connectToDatabase();
-       const [rows] = await connection.query(`SELECT * FROM cuponsusados WHERE idcliente = ?`, [userId]);
-       return rows;
+        const connection = await connectToDatabase();
+        const query = `
+            SELECT cuponsusados.*, restaurantes.nome AS restauranteNome
+            FROM cuponsusados
+            JOIN restaurantes ON cuponsusados.idrestaurante = restaurantes.id
+            WHERE cuponsusados.idcliente = ? AND cuponsusados.usado = 1
+        `;
+        const [rows] = await connection.query(query, [userId]);
+        return rows;
     } catch (error) {
-        throw new Error(error)
+        throw new Error(error);
     }
 }
 
 module.exports = {
     getAllUsedCupons,
     getAllUsedCuponsByUserId
-}
+};
